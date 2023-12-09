47cd7a11d3e5cfb0edf98b7d32ac647460437429
oromero7153 <131738715+oromero7153@users.noreply.github.com>
2023-12-09 14:15:02 -0800

    updated schema

--- server/models/schemas.js
+++ server/models/schemas.js
@@ -1,8 +1,13 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const ridesSchema = new Schema({
-
+const taskSchema = new Schema({
+    photo: { type: String },
+    name: { type: String, required: true },
 });
+
+const Task = mongoose.model("Task", taskSchema, "task");
+
+const mySchemas = {Task: Task}
 
 module.exports = mySchemas;
