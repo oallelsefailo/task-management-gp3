6420a533c20c7012af18d30254aeb940abab901c
Brandon <oallelsefailo@gmail.com>
2023-12-10 15:15:16 -0800

    Put the login page to be handled first then redirected to /collections. Following routes should only display once the user is logged in

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,29 @@
+import React, { useState } from "react";
 import "./App.css";
 import Navigation from "./components/Navigation";
 import Container from "./components/Container";
+import LoginPage from "./components/LoginPage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
+  const [isLoggedIn, setLoggedIn] = useState(false);
+
+  const handleLogin = () => {
+    setLoggedIn(true);
+  };
+
   return (
     <Router>
       <div className="app-container">
-        <Navigation />
+        {isLoggedIn && <Navigation />}
         <Routes>
-          <Route path="/" element={<Container />} />
+          <Route
+            path="/collections"
+            element={
+              isLoggedIn ? <Container /> : <LoginPage onLogin={handleLogin} />
+            }
+          />
+          <Route path="/" element={<LoginPage onLogin={handleLogin} />} />
         </Routes>
       </div>
     </Router>
