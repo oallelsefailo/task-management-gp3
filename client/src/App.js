01c4f70b6fec9c175fa34b43cee86f3a8c3ef404
jorodrig96 <jo.rodrig96@gmail.com>
2023-12-11 19:58:38 -0800

    got the splash page to run

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,58 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Navigation from "./components/Navigation";
 import Container from "./components/Container";
-import LoginPage from "./components/LoginPage";
+// import LoginPage from "./components/LoginPage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import PacmanLoader from "react-spinners/PacmanLoader";
+
 
 function App() {
+  const [loading, setLoading] = useState(false);
   const [isLoggedIn, setLoggedIn] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 5000);
+  }, []);
 
   const handleLogin = () => {
     setLoggedIn(true);
   };
 
   return (
-    <Router>
-      <div className="app-container">
-        {isLoggedIn && <Navigation />}
-        <Routes>
-          <Route
-            path="/collections"
-            element={
-              isLoggedIn ? <Container /> : <LoginPage onLogin={handleLogin} />
-            }
-          />
-          <Route path="/" element={<LoginPage onLogin={handleLogin} />} />
-        </Routes>
-      </div>
-    </Router>
+    <>
+      {loading ? (
+        <PacmanLoader 
+          color={"#36D7B7"}  
+          loading={loading}
+          size={30}
+          className="pacManLoader"
+        />
+      ) : (
+        <>
+          <Router>
+            <div className="app-container">
+              {isLoggedIn && <Navigation />}
+              <Routes>
+                {/* 
+                  <Route
+                    path="/collections"
+                    element={
+                      isLoggedIn ? <Container /> : <LoginPage onLogin={handleLogin} />
+                    }
+                  />
+                  <Route path="/" element={<LoginPage onLogin={handleLogin} />} />
+                */}
+              </Routes>
+            </div>
+          </Router>
+          <h1>EvilMonday</h1>
+        </>
+      )}
+    </>
   );
 }
 
