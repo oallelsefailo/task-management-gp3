18d1801e8745d8731a7b1c983a5a7b64a160f9af
Manny Delgado <119555756+MannyGDP@users.noreply.github.com>
2023-12-19 00:56:05 -0800

    "Working error page"

--- client/src/components/SignupLogin.js
+++ client/src/components/SignupLogin.js
@@ -6,14 +6,11 @@ export default function SignupLogin() {
     const navigate = useNavigate();
 
     const handleLoginClick = () => {
-        // Simulate a condition where login is not added
         const loginNotAdded = true;
 
         if (loginNotAdded) {
-            // If login is not added, navigate to the error page
-            navigate("./components/Error404"); // Replace '/error' with the path of your error page
+            navigate("./components/Error404");
         } else {
-            // If login is added, navigate to the login page
             navigate("./components/LoginPage");
         }
     };
