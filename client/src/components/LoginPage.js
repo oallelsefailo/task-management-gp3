a97de466f0ebedca3b778207d720358f61274562
jorodrig96 <jo.rodrig96@gmail.com>
2023-12-18 19:38:14 -0800

    all signup/login info is required before having to sign in. need to connect to db now

--- client/src/components/LoginPage.js
+++ client/src/components/LoginPage.js
@@ -1,8 +1,28 @@
 import "../components/loginpage.css"
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 export default function LoginPage({ onLogin }) {
   const navigate = useNavigate();
+  
+  const [ userCredentials, setUserCredentials ] = useState({
+    email: '',
+    password: ''
+  })
+
+  async function handleSubmit(e) {
+    e.preventDefault()
+
+    await fetch('http://localhost:3000/LoginPage', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    }, 
+    body: JSON.stringify(userCredentials)
+    })
+    navigate('/task')
+  }
+
   return (
     <>
       <div className="logInPage">
@@ -11,27 +31,36 @@ export default function LoginPage({ onLogin }) {
           <div className="labelInput">
             <label>Villain UserName:</label>
             <input
+            required
+              value={userCredentials.email}
+              onChange={e => setUserCredentials({ ...userCredentials, email: e.target.value})}
               className="inputbar"
               type="email"
               placeholder="enter email"
+              id='email'
+              name='email'
             />
           </div>
           <br />
           <div className="labelInput">
             <label>Secret Password:</label>
             <input
+            required
+              value={userCredentials.password}
+              onChange={e => setUserCredentials({ ...userCredentials, password: e.target.value})}
               className="inputbar"
               type="password"
               placeholder="password"
+              id= 'password'
+              name= 'password'
             />
             <label className="rightsidetext">
               (enter correctly to get into lair)
             </label>
           </div>
-        </form>
+        
         <div>
-          <a href="/task" />
-          <button className="loginButton" onClick={() => navigate("/task")}>
+          <button className="loginButton" type='submit' Value='logged in '>
             Enter the Evil Lair
           </button>
           <a href="/" />
@@ -39,6 +68,7 @@ export default function LoginPage({ onLogin }) {
             Return to sign up{" "}
           </button>
         </div>
+        </form>
       </div>
     </>
   );
