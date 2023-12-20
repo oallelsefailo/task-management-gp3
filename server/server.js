4dcb62c86a45e71cb4b882b91c3540a0a9f611a2
jorodrig96 <jo.rodrig96@gmail.com>
2023-12-20 07:46:06 -0800

    added bcrypt as a dependency to both client and server. Called bcrypt on the server.js file and added a function to hash

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt");
 const signUpRouter = require("./routes/SignupPage");
 const submitTaskRouter = require("./routes/submit_task");
 const submitAssignmentRouter = require("./routes/submit_assignment");
@@ -22,6 +23,18 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
+
+app.use("/SignupPage", async (req, res, next) => {
+  try {
+    const { password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 12);
+    req.body.password = hashedPassword;
+    next();
+  } catch (error) {
+    console.error('Error hashing password to DB', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
 
 app.use("/submit_task", submitTaskRouter);
 app.use("/submit_assignment", submitAssignmentRouter);
